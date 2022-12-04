46a9925f4e3d8209e0077c5c599778c5e39b02c1
Naganathan <93819633+Nagavshiva@users.noreply.github.com>
2022-12-04 23:44:10 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,140 @@
+import express from 'express';
+import data from './data.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import { dirname } from 'path';
+import productRouter from './routes/productRouter.js';
+import userRouter from './routes/userRouter.js';
+import orderRouter from './routes/orderRouter.js';
+import uploadRouter from './routes/uploadRouter.js';
+import http from 'http';
+import { Server } from 'socket.io';
+
+dotenv.config();
+
+// db connection
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('connected to db');
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+ 
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get('/api/config/paypal', (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
+});
+
+
+
+
+
+
+// routes
+app.use('/api/uploads', uploadRouter);
+app.use('/api/products', productRouter);
+app.use('/api/users', userRouter);
+app.use('/api/orders', orderRouter);
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+
+const port = process.env.PORT || 5000;
+const httpServer = http.Server(app);
+const io = new Server(httpServer, { cors: { origin: '*' } });
+const users = [];
+
+
+
+io.on('connection', (socket) => {
+  console.log('connection', socket.id);
+  socket.on('disconnect', () => {
+    const user = users.find((x) => x.socketId === socket.id);
+    if (user) {
+      user.online = false;
+      console.log('Offline', user.name);
+      const admin = users.find((x) => x.isAdmin && x.online);
+      if (admin) {
+        io.to(admin.socketId).emit('updateUser', user);
+      }
+    }
+  });
+  socket.on('onLogin', (user) => {
+    const updatedUser = {
+      ...user,
+      online: true,
+      socketId: socket.id,
+      messages: [],
+    };
+    const existUser = users.find((x) => x._id === updatedUser._id);
+    if (existUser) {
+      existUser.socketId = socket.id;
+      existUser.online = true;
+    } else {
+      users.push(updatedUser);
+    }
+    console.log('Online', user.name);
+    const admin = users.find((x) => x.isAdmin && x.online);
+    if (admin) {
+      io.to(admin.socketId).emit('updateUser', updatedUser);
+    }
+    if (updatedUser.isAdmin) {
+      io.to(updatedUser.socketId).emit('listUsers', users);
+    }
+  });
+
+  socket.on('onUserSelected', (user) => {
+    const admin = users.find((x) => x.isAdmin && x.online);
+    if (admin) {
+      const existUser = users.find((x) => x._id === user._id);
+      io.to(admin.socketId).emit('selectUser', existUser);
+    }
+  });
+
+  socket.on('onMessage', (message) => {
+    if (message.isAdmin) {
+      const user = users.find((x) => x._id === message._id && x.online);
+      if (user) {
+        io.to(user.socketId).emit('message', message);
+        user.messages.push(message);
+      }
+    } else {
+      const admin = users.find((x) => x.isAdmin && x.online);
+      if (admin) {
+        io.to(admin.socketId).emit('message', message);
+        const user = users.find((x) => x._id === message._id && x.online);
+        user.messages.push(message);
+      } else {
+        io.to(socket.id).emit('message', {
+          name: 'Admin',
+          body: 'Sorry. I am not online right now',
+        });
+      }
+    }
+  });
+});
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+// server static assets if in production
+if(process.env.NODE_ENV === 'production'){    
+  app.use(express.static('client/build'))  // set static folder 
+  //returning frontend for any route other than api 
+  app.get('*',(req,res)=>{     
+      res.sendFile (path.resolve(__dirname,'client','build','index.html' ));    
+  });
+}
+
+
+httpServer.listen(port, () => {
+  console.log(`Serve at http://localhost:${port}`);
+});
